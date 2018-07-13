5bd8f4d2a014ecd5b32ce68bae4bbca947001735
Adriana Disano <adrianadisano@gmail.com>
2018-07-13 17:14:12 -0500

    Add project files.

---
+++ StringAddition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
